5380d9a6c2add68a96a9a6dbb502d4741e196f3d
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-05-10 23:43:23 +0530

    Create addition_under_modulo.cpp

---
+++ gfg/addition_under_modulo.cpp
@@ -0,0 +1,35 @@
+// https://practice.geeksforgeeks.org/problems/addition-under-modulo/1/
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        long long sumUnderModulo(long long a,long long b)
+    {
+        // code here
+        long long mod = 1000000007;
+        long long ans = ((a%mod)+(b%mod))%mod;
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
