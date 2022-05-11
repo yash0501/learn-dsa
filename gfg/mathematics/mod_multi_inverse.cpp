cbe95262a76dad50af15af39c0c4eedaa9dbba48
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-05-11 14:31:20 +0530

    Create mod_multi_inverse.cpp

---
+++ gfg/mathematics/mod_multi_inverse.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Complete this function
+    int modInverse(int a, int m)
+    {
+        //Your code here
+        int x=1;
+        while((x*a)%m!=1 && x<m){
+            x++;
+        }
+        if(x>=m){
+            return -1;
+        }
+        return x;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--){
+		int a,m;
+		
+		//taking input a and m
+		cin>>a>>m;
+		Solution ob;
+		//calling function modInverse()
+		cout << ob.modInverse(a, m)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
