fc685c6111ff75e14accd3681d5dfc1d03ec7172
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-05-15 11:47:35 +0530

    Create immediate_smaller_than_x.cpp

---
+++ gfg/arrays/immediate_smaller_than_x.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // User function to complete
+    // arr[]: input array
+    // n: size of array
+    // k: find element smaller and closest to it. 
+    int immediateSmaller(int arr[], int n, int x)
+    {
+        // your code here
+        int ans=-1, diff=INT_MAX, i;
+        for(i=0;i<n;i++){
+            if(x-arr[i]<diff && x-arr[i]>0){
+                diff=x-arr[i];
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    
+            int n;
+        	cin >> n;
+        	
+        	int arr[n];
+        	
+        	for(int i = 0;i<n;i++){
+        	    cin >> arr[i];
+        	}
+        	
+        	int x;
+        	cin >> x;
+        	Solution ob;
+        	cout << ob.immediateSmaller(arr, n, x) << endl;
+	}
+	
+	
+	return 0;
+}
+  // } Driver Code Ends
