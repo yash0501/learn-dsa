8aa80b5d7e41d35ec7bacc8bc4247fdbcdfe6e6a
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-05-23 23:56:05 +0530

    Create quick_sort.cpp

---
+++ gfg/sorting_algorithm/quick_sort.cpp
@@ -0,0 +1,2 @@
+/* In quick sort, we select an element from array as pivot and then arrange it at its fixed position with elements smaller than it being towards its left and greater towards its right.
+*/
