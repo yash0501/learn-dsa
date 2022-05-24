9fd986af736d2c3a73201be64f691f738a27205e
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-05-24 11:00:07 +0530

    Update quick_sort.cpp

--- gfg/sorting_algorithm/quick_sort.cpp
+++ gfg/sorting_algorithm/quick_sort.cpp
@@ -1,2 +1,66 @@
-/* In quick sort, we select an element from array as pivot and then arrange it at its fixed position with elements smaller than it being towards its left and greater towards its right.
+/* In quick sort, we select an element from array as pivot and then arrange it at its fixed position with elements smaller than it being towards its left,
+and greater towards its right.
 */
+
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+ // } Driver Code Ends
+//User function Template for C
+
+//Function to sort an array using quick sort algorithm.
+void quickSort(int arr[], int low, int high)
+{
+    // code here
+    if(low<high){
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p-1);
+        quickSort(arr, p+1, high);
+    }
+}
+    
+int partition (int arr[], int low, int high)
+{
+   // Your code here
+   int pivot=arr[high];
+   int i=low-1;
+   int j, t;
+   for(j=low;j<=high-1;j++){
+       if(arr[j]<=pivot){
+           i++;
+           t=arr[i];
+           arr[i]=arr[j];
+           arr[j]=t;
+       }
+   }
+   t=arr[i+1];
+   arr[i+1]=arr[high];
+   arr[high]=t;
+   return i+1;
+}
+
+// { Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}  // } Driver Code Ends
