e35fcb1f32e73fb2743e32ffd4e7569efd6b2570
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-08-26 19:35:41 +0530

    Create n_to_1.cpp

---
+++ recursion/n_to_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<endl;
+    fun(n-1);
+}
+
+int main() {
+	int n;
+	cin>>n;
+	fun(n);
+	return 0;
+}
