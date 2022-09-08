2b2cf0b102a62f4d74470d8d80f45489043ba765
Yash Garg <60708843+yash0501@users.noreply.github.com>
2022-09-08 23:54:25 +0530

    Create delete_node_in_tree.cpp

---
+++ trees/delete_node_in_tree.cpp
@@ -0,0 +1,118 @@
+// C++ program to delete element in binary tree
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has key, pointer to left
+child and a pointer to right child */
+struct Node {
+	int key;
+	struct Node *left, *right;
+};
+
+/* function to create a new node of tree and
+return pointer */
+struct Node* newNode(int key)
+{
+	struct Node* temp = new Node;
+	temp->key = key;
+	temp->left = temp->right = NULL;
+	return temp;
+};
+
+/* Inorder traversal of a binary tree*/
+void inorder(struct Node* temp)
+{
+	if (!temp)
+		return;
+	inorder(temp->left);
+	cout << temp->key << " ";
+	inorder(temp->right);
+}
+
+/* function to delete the given deepest node
+(d_node) in binary tree */
+void deletDeepest(struct Node* root, struct Node* d_node){
+    if(root == NULL){
+        return;
+    }
+	if(root==d_node){
+	    delete d_node;
+	    root=NULL;
+	    return;
+	}
+	if(root->left==d_node){
+	    delete d_node;
+	    root->left=NULL;
+	    return;
+	}
+	if(root->right==d_node){
+	    delete d_node;
+	    root->right=NULL;
+	    return;
+	}
+	deletDeepest(root->left, d_node);
+	deletDeepest(root->right, d_node);
+}
+
+/* function to delete element in binary tree */
+Node* deletion(struct Node* root, int key){
+	if(root == NULL){
+	    return NULL;
+	}
+	if(root->left==NULL&&root->right==NULL){
+	    if(root->key==key){
+	        return NULL;
+	    }
+	    else{
+	        return root;
+	    }
+	}
+	queue<Node *> q;
+	q.push(root);
+	Node * key_node = NULL;
+	Node * last_node = NULL;
+	Node * t;
+	while(!q.empty()){
+	    t = q.front();
+	    q.pop();
+	    if(t->key==key){
+	        key_node=t;
+	    }
+	    if(t->left!=NULL){
+	        q.push(t->left);
+	    }
+	    if(t->right!=NULL){
+	        q.push(t->right);
+	    }
+	}
+	last_node = t;
+	if(key_node){
+	    key_node->key = last_node->key;
+	    deletDeepest(root, last_node);
+	}
+	return root;
+}
+
+// Driver code
+int main()
+{
+	struct Node* root = newNode(10);
+	root->left = newNode(11);
+	root->left->left = newNode(7);
+	root->left->right = newNode(12);
+	root->right = newNode(9);
+	root->right->left = newNode(15);
+	root->right->right = newNode(8);
+
+	cout << "Inorder traversal before deletion : ";
+	inorder(root);
+
+	int key = 11;
+	root = deletion(root, key);
+
+	cout << endl;
+	cout << "Inorder traversal after deletion : ";
+	inorder(root);
+
+	return 0;
+}
